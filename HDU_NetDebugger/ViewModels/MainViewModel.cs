3a6f58604c6371d423900551da7b850ee00fc6e9
Yue-bin <3103174284@qq.com>
2025-10-25 01:36:59 +0800

    ✨ feat: run button

--- HDU_NetDebugger/ViewModels/MainViewModel.cs
+++ HDU_NetDebugger/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using HDU_NetDebugger.Services;
+using System;
 using System.Collections.ObjectModel;
+using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 
 namespace HDU_NetDebugger.ViewModels;
@@ -17,6 +19,25 @@ public partial class MainViewModel : ViewModelBase
                 CheckItems.Add(new CheckItemViewModel(checker.Name, checker, conditions));
             }
         }
+        RunChecksCommand.NotifyCanExecuteChanged();
+        Console.WriteLine($"Loaded {CheckItems.Count} checkers.");
     }
     public ObservableCollection<CheckItemViewModel> CheckItems { get; } = [];
+
+    [RelayCommand(CanExecute = nameof(CanRunChecks))]
+    public async Task RunChecks()
+    {
+        Console.WriteLine("Running checks...");
+        IsRunningChecks = true;
+        foreach (var item in CheckItems)
+            await item.RunAsync();
+        IsRunningChecks = false;
+    }
+    public bool CanRunChecks() => HasItems && !IsRunningChecks;
+    private bool HasItems => CheckItems?.Count > 0;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(RunChecksCommand))]
+    public bool isRunningChecks = false;
+
 }
