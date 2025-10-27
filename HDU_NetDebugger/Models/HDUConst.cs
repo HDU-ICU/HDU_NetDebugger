e2b70283c4ec7993e177e0082e0e090d786528fd
Yue-bin <3103174284@qq.com>
2025-10-27 14:55:44 +0800

    🐞 fix: wrong cidr

--- HDU_NetDebugger/Models/HDUConst.cs
+++ HDU_NetDebugger/Models/HDUConst.cs
@@ -7,5 +7,5 @@ public static class HDUConst
     // 生活区网段
     public static readonly IPNetwork DormNetwork = IPNetwork.Parse("10.150.0.0/16");
     // 校园网段
-    public static readonly IPNetwork CampusNetwork = IPNetwork.Parse("10.149.0.0/16");
+    public static readonly IPNetwork CampusNetwork = IPNetwork.Parse("10.249.0.0/16");
 }
